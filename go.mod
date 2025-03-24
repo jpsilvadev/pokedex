191d7f5ce95d26de0990d65f291c181367e61bff
jpsilvadev <jpsjoaosilva98@gmail.com>
2025-03-24 16:57:28 +0000

    feat: help and exit commands

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jpsilvadev/pokedex
 
-go 1.24
+go 1.22
--- repl.go
+++ repl.go
@@ -12,11 +12,25 @@ func startRepl() {
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
+
 		text := scanner.Text()
 		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
 
-		command := words[0]
-		fmt.Println("Your command was:", command)
+		commandPovided := words[0]
+		command, exists := getCommands()[commandPovided]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
@@ -24,4 +38,25 @@ func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	words := strings.Fields(lowerText)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
